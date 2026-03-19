1a8ab61799e20e2c76c154a24fe00662d40502c3
Rahul-404 <rahulshelke981@gmail.com>
2026-03-19 19:59:29 +0530

    solved typo and util.py bug

--- src/utils.py
+++ src/utils.py
@@ -11,11 +11,13 @@ from src.exception import CustomException
 
 def save_object(file_path:str, obj):
     try:
-        dir_path = os.path.join(file_path)
-
+        # Correctly get only the folder path
+        dir_path = os.path.dirname(file_path)
+        # Create the directory if it doesn't exist
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+            
     except Exception as e:
         raise CustomException(e, sys)
